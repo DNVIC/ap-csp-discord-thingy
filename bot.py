6035ed86e90f5438f37bbf37cee92eed335da175
DNVIC <37513362+DNVIC@users.noreply.github.com>
2019-10-07 13:38:06 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+
+
+client = MyClient()
+client.run('token goes here')
